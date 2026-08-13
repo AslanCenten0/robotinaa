b5c8bd9f39e375d1e642055aee244c32a19e834e
Angel Ruggia Dufour <aruggia@colegium.com>
2026-08-13 09:44:54 -0300

    fix: update hand landmarker model path to the standard task version

--- src/components/CameraTranslator.tsx
+++ src/components/CameraTranslator.tsx
@@ -54,7 +54,7 @@ const CameraTranslator: React.FC = () => {
         const landmarker = await HandLandmarker.createFromOptions(vision, {
           baseOptions: {
             modelAssetPath:
-              'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker_full/float16/latest/hand_landmarker_full.task',
+              'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task',
             delegate: 'GPU',
           },
           runningMode: 'VIDEO',
